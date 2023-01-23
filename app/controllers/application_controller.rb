f41b928a8f20dc3e93c9173263eb842827638337
EC2 Default User <ec2-user@ip-172-31-19-246.ap-northeast-1.compute.internal>
2023-01-23 15:12:28 +0000

    [Add]ヘッダーのレイアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,13 @@ class ApplicationController < ActionController::Base
     about_path
   end
 
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
   end
 end
